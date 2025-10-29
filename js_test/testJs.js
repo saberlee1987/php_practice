0267850672bfa2984c7ac93e12dfba3329a110a3
azizi.s <aziz.s@tiddev.com>
2025-10-29 11:52:07 +0330

    practice php and js

--- js_test/testJs.js
+++ js_test/testJs.js
@@ -1,3 +1,18 @@
+let array = [];
+array.push(1, 2, 3, 4, 5, 6, 7);
+array.pop();
+array = array.concat([7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]);
+array.forEach((value, i) => console.log(i + " ==> " + value));
+console.log(array instanceof Array);
+console.log(Array.isArray(array));
+console.log(array.toString());
+console.log(array.join("-"));
+console.log(array.filter(value => {return value % 2 === 0}).toString())
+console.log(array.map(value => {return value * 2}).toString())
+console.log(array.sort((v1,v2)=>{return v2 - v1}).toString()) //desc
+console.log(array.sort((v1,v2)=>{return v1 - v2}).toString()) //asc
+
+
 function test() {
     document.getElementById("demo")
         .innerHTML = "hello,I am an output";
@@ -21,8 +36,8 @@ function calculator() {
 
 function calculate(number1, number2, operator) {
     var result = 0;
-     number1 = parseFloat(number1);
-     number2 = parseFloat(number2);
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
     switch (operator) {
         case "+":
             result = number1 + number2;
@@ -31,7 +46,7 @@ function calculate(number1, number2, operator) {
             result = number1 * number2;
             break;
         case "/":
-            result = number1/ number2;
+            result = number1 / number2;
             break;
         case "-":
             result = number1 - number2;
@@ -43,4 +58,14 @@ function calculate(number1, number2, operator) {
         result = `${number1} ${operator} ${number2} = ${result.toFixed(2)}`;
     }
     return result;
+}
+
+function mouseover(event) {
+    console.log(event);
+    console.log("mouseover event");
+}
+
+function changer(event) {
+    console.log(event);
+    console.log("changer event");
 }
--- php_test/index2.php
+++ php_test/index2.php
@@ -1,3 +1,12 @@
 <?php
 //var_dump($_SERVER);
-var_dump($_GET);
+//var_dump($_GET);
+session_start([
+    "name"=> "saber_sessionId"
+]);
+$_SESSION["token"] = "wesdflgsdfse";
+$_SESSION["auth"] = true;
+$_SESSION["name"] = "saber";
+ //setcookie("theme","light",time() + 6000 , "/","localhost",false,true);
+var_dump($_SESSION);
+//var_dump($_COOKIE);

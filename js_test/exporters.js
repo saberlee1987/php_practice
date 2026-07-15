8674e942043ca0477243b4d7826a00f70bf4d65f
azizi.s <aziz.s@tiddev.com>
2026-07-15 16:00:24 +0330

    practice  javascript 7

---
+++ js_test/exporters.js
@@ -0,0 +1,11 @@
+let firstname = "saber"
+let lastname = "azizi"
+
+// export function setFirstname(fname) {
+//     firstname = fname;
+// }
+//
+// export function setLastname(lname) {
+//   lastname = lname;
+// }
+export let fullname = `${firstname} ${lastname}`
---
+++ js_test/modules1.js
@@ -0,0 +1,7 @@
+import {fullname} from "./exporters.js";
+// import {setFirstname,setLastname} from "./exporters.js";
+
+console.log(fullname);
+// setFirstname("bruce");
+// setLastname("lee")
+// console.log(fullname);
